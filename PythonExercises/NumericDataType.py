9bb0e2a58ec94f6c518a56b03007332e6f98e28a
imtanmay98 <imrout25@gmail.com>
2024-08-19 01:07:25 +0530

    NumericDataTypes

---
+++ PythonExercises/NumericDataType.py
@@ -0,0 +1,33 @@
+# The most basic datatypes are the numeric, string & boolean
+"""
+Numeric types:
+1. integer
+2. Float
+3. Complex
+"""
+########################################
+"""
+Integer:
+********
+1. The type of data that is positive or negetive whole number are integer type
+2. This is reprented as "int"
+Ex: 
+"""
+number = 5
+age = 26
+###################################
+"""
+Float:
+EX:
+******
+"""
+temprature = 34.5
+rateOfinterest = 13.75
+########################################
+"""
+Complex:
+ it should be expressed in the form of a+bi, where 'a & b' are real part & 'i' is imaginary part
+
+num = 5+3i
+print(num)
+"""
