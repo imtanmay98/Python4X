345e8295fd36f69be92455918444e78522ee4417
imtanmay98 <imrout25@gmail.com>
2024-08-19 00:32:08 +0530

    Variables in Python

---
+++ PythonExercises/Variables.py
@@ -0,0 +1,19 @@
+"""
+Data is stored in the form of memory & when we label it we call this label as variable
+for ex lets asume a a glass of milk & we put a sticker o the glass as "MILK"
+here consider the milk in the glass is data, 7 the sticker of "MILK" is variable
+"""
+# We can declare the variable using an Assignment operator
+# Ex, lets say we hv an app that needs to store the name of the countries
+country = "INDIA"
+print(country)
+
+# Rules neededto follow whiledeclaring a variable,
+"""
+1. A variable name should be meaningfull & descriptive
+2. A variable namemust start with a latter orunderscore character
+3. Variable name can not start with number
+4. A variable name can only contain alfa-numeric number and underscore
+5. A variable name in python are case senstive means 'country' & 'COUNTRY' are different
+6. A value, if word or character, hass to be enclosed in double or single cquotes
+"""
