5ec5020839e41d3a3a702634202645992394234b
imtanmay98 <imrout25@gmail.com>
2024-08-19 01:29:21 +0530

    type() funstion

---
+++ PythonExercises/type()func.py
@@ -0,0 +1,13 @@
+# There areinstances when we want to know the type of data stored
+# Very helpful for debugging
+"""
+syntax:
+type()
+"""
+# example,
+name = "Tanmay"
+age = 26
+Weight = 77.4
+print(type(name)) # string type is reflected as str
+print(type(age)) # int
+print(type(Weight)) # float
