04786d1b443f2f2e41575840fd765b57f3809b24
imtanmay98 <imrout25@gmail.com>
2024-08-19 00:07:57 +0530

    Comments in Python

---
+++ PythonExercises/comments.py
@@ -0,0 +1,20 @@
+# Comments are ussed to describe what each part of your code does.
+# it's important when your code is more than one line
+# To sum up comments are used to describe our programme
+# This '#' is used for stating a comment 7 they are ignored by python
+
+# for ex below is to print hello world
+print("hello world")
+
+# Similarly, there is multi-line comment
+# Used when we want to comment mutiple line at once
+# Here is an example of multi-line comment
+"""
+print("I")
+print("am")
+"""
+print("Tanmay")
+print("Kumar")
+print("Rout")
+# But if we don't use the second occurrence of triple quote'"""',
+#It will consider all the rest of the codes after the first occurrance as comment, so be careful
