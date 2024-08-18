1f3b94a4165b71bfa471eda0458aef95fa7087a9
imtanmay98 <imrout25@gmail.com>
2024-08-19 00:48:45 +0530

    Introduction of data types

---
+++ PythonExercises/DataTypesIntro.py
@@ -0,0 +1,17 @@
+"""
+A variable is used to store the data required by the application,
+that data can be a word, number or decimal etc,
+"""
+# Ex,
+
+name = "Tanmay"
+age = 26
+height = 5.8
+# here we can see 3 variables & all three of them contains different types of values
+print(name)
+print(age)
+print(height)
+# This brings us to the concept of "Data Types"
+"""
+In python data types are catagorised as  Numeric Type, Sequencial type ,
+Boolean tTpe ,Sets & Mapping Type(Dictonary)
