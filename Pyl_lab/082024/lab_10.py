75f6e2560986594e88198105c73c90f6f826755d
imtanmay98 <imrout25@gmail.com>
2024-09-30 00:44:25 +0530

    Variable

---
+++ Pyl_lab/082024/lab_10.py
@@ -0,0 +1,30 @@
+# when creating the variable things to watch
+"""
+1. type of the container/variable
+2. name of the container
+3. value stored in the container
+"""
+#python Data types
+"""
+1. Numeric
+ a. integer(int) e.g -1, -2, 2 ,3 ,4, 000099999222
+ b. float eg. 3.12, 5.44
+ c. complex e.g. -1+2j --> j= root of -1
+ d. boolean ---> True/False
+
+2. String
+ a. represted as 'str'
+ b. bunch of character
+3. list
+  list = ["milk", "bread"]
+  my_marks=["Tanmay", 95, 23]
+4. Advance data types
+  a. set, dict, tuple, binary, frozen set.
+"""
+#variable name should always start with string or underscore
+pi = 3.14
+name = "Tanmay"
+isMale = True
+print(type(pi))
+print(type(name))
+print(type(isMale))
