cb9c34a38fda37c7d9daee167e922e71c3cd01e5
imtanmay98 <imrout25@gmail.com>
2024-09-29 23:54:32 +0530

    list & it's operations

---
+++ Pyl_lab/082024/lab_02.py
@@ -0,0 +1,6 @@
+# python prog
+print("Tanmay", 1234, 3.22, True, sep="-", end="_")
+print("Tanmay")
+# these are the built in utility provided by python
+# sep - string insert between the values in print
+# end - string append after the last vaue
