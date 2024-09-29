5ade602195d459b328a57ede625c34cb3adc38a9
imtanmay98 <imrout25@gmail.com>
2024-09-30 00:22:56 +0530

    Basics

---
+++ Pyl_lab/082024/lab_08.py
@@ -0,0 +1,9 @@
+# keywords & identifiers
+# keywords are the reserve words used by python
+# can't use keywords as variable names & all the keywords are case sensitive
+
+import keyword
+
+print(keyword.kwlist, sep="\n")
+
+
