1d737dfc7b0597e876664b0f5be8ffdc340675fd
imtanmay98 <imrout25@gmail.com>
2024-09-30 00:53:09 +0530

    Variable

---
+++ Pyl_lab/082024/lab_11.py
@@ -0,0 +1,15 @@
+#Dynamic type
+# at the run time the variable can be changed & you dont need to mention the data type
+#python interpreter is very smart
+age =20
+age= 25
+print(age)
+log_variable_language = "tanmay"
+print(log_variable_language)
+a= 23
+b=4
+c=5
+sum = a+b+c
+sum= sum-6
+
+print(sum)
